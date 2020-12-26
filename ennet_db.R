461d03124e3ebd04cf137a1b4e368b7e8fd9206e
Ernest Guevarra <ernest@guevarra.io>
2020-12-26 19:15:27 +0000

    update extract function

--- ennet_db.R
+++ ennet_db.R
@@ -1,8 +1,15 @@
 ## Libraries
-if(!require(ennet)) install.packages("ennet")
 if(!require(remotes)) install.packages("remotes")
-remotes::install_github("katilingban/ennet@dev")
 if(!require(magrittr)) install.packages("magrittr")
+if(!require(rvest)) install.packages("rvest")
+if(!require(xml2)) install.packages("xml2")
+if(!require(tibble)) install.packages("tibble")
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(stringr)) install.packages("stringr")
+if(!require(lubridate)) install.packages("lubridate")
+
+if(!require(ennet)) install.packages("ennet")
+remotes::install_github("katilingban/ennet@dev")
 
 ## 
 x <- ennet::get_themes() %>%
