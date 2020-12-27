780758cfe91e27b02bdf7d6cba2b7b08a2980a81
Ernest Guevarra <ernest@guevarra.io>
2020-12-27 16:30:32 +0000

    update workflow

--- ennet_topics.R
+++ ennet_topics.R
@@ -1,15 +1,15 @@
 ## Libraries
-#if(!require(remotes)) install.packages("remotes")
-#if(!require(magrittr)) install.packages("magrittr")
-#if(!require(rvest)) install.packages("rvest")
-#if(!require(xml2)) install.packages("xml2")
-#if(!require(tibble)) install.packages("tibble")
-#if(!require(dplyr)) install.packages("dplyr")
-#if(!require(stringr)) install.packages("stringr")
-#if(!require(lubridate)) install.packages("lubridate")
+if(!require(remotes)) install.packages("remotes")
+if(!require(magrittr)) install.packages("magrittr")
+if(!require(rvest)) install.packages("rvest")
+if(!require(xml2)) install.packages("xml2")
+if(!require(tibble)) install.packages("tibble")
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(stringr)) install.packages("stringr")
+if(!require(lubridate)) install.packages("lubridate")
 
-#if(!require(ennet)) install.packages("ennet")
-#remotes::install_github("katilingban/ennet")
+if(!require(ennet)) install.packages("ennet")
+remotes::install_github("katilingban/ennet")
 
 ## 
 x <- ennet::get_themes() %>%
