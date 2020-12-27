985b93437a161790a553547d9dc8444b0f76976a
Ernest Guevarra <ernest@guevarra.io>
2020-12-27 09:02:17 +0000

    create ennet_topics.R to extract ennet topics

---
+++ ennet_topics.R
@@ -0,0 +1,28 @@
+## Libraries
+if(!require(remotes)) install.packages("remotes")
+if(!require(magrittr)) install.packages("magrittr")
+if(!require(rvest)) install.packages("rvest")
+if(!require(xml2)) install.packages("xml2")
+if(!require(tibble)) install.packages("tibble")
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(stringr)) install.packages("stringr")
+if(!require(lubridate)) install.packages("lubridate")
+
+if(!require(ennet)) install.packages("ennet")
+remotes::install_github("katilingban/ennet")
+
+## 
+x <- ennet::get_themes() %>%
+  ennet::get_themes_topics()
+
+## Get current date and time
+current_date_time <- Sys.time() %>%
+  stringr::str_replace_all(pattern = " ", replacement = "_")
+
+##
+write.csv(x = x,
+          file = paste("data/ennet_topics_",
+                       current_date_time,
+                       ".csv",
+                       sep = ""),
+          row.names = FALSE)
