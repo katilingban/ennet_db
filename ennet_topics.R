d5d2c98684f4ef5139ed8e7573f657effbc7cdf2
Ernest Guevarra <ernest@guevarra.io>
2020-12-31 22:18:04 +0000

    update script for hourly dataset

--- ennet_topics.R
+++ ennet_topics.R
@@ -11,18 +11,20 @@ if(!require(lubridate)) install.packages("lubridate")
 if(!require(ennet)) install.packages("ennet")
 remotes::install_github("katilingban/ennet")
 
+##
+x <- read.csv(file = "data/ennet_topics.csv")
+
 ## 
-x <- ennet::get_themes() %>%
+y <- ennet::get_themes() %>%
   ennet::get_themes_topics()
 
-## Get current date and time
-current_date_time <- Sys.time() %>%
-  stringr::str_replace_all(pattern = " ", replacement = "_")
+##
+y <- y[c(1, 2, 4, 5, 6, 3, 7)]
+
+##
+x <- rbind(x, y)
 
 ##
 write.csv(x = x,
-          file = paste("data/ennet_topics_",
-                       current_date_time,
-                       ".csv",
-                       sep = ""),
+          file = "data/ennet_topics.csv",
           row.names = FALSE)
